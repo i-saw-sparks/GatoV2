173a1b47a0d13856a40e92634afe0d92c91a492b
i-saw-sparks <andresmolinaceti@gmail.com>
2018-05-10 18:12:48 -0500

    Se le ingreso id a cada boton y se relazionaron con variables en el archivo java

--- app/src/main/java/com/example/andrs/gato/MainActivity.java
+++ app/src/main/java/com/example/andrs/gato/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.andrs.gato;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+public Button b1, b2, b3, b4, b5, b6, b7, b8, b9
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.b1);
+        b2=(Button)findViewById(R.id.b2);
+        b3=(Button)findViewById(R.id.b3);
+        b4=(Button)findViewById(R.id.b4);
+        b5=(Button)findViewById(R.id.b5);
+        b6=(Button)findViewById(R.id.b6);
+        b7=(Button)findViewById(R.id.b7);
+        b8=(Button)findViewById(R.id.b8);
+        b9=(Button)findViewById(R.id.b9);
     }
 }
